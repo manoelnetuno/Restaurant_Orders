3bff9ce381fbb0d1ba80aff4ed511891fcc06873
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-12-12 15:02:27 -0300

    testes de ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    # Setup: Criação dos ingredientes
+    ingredient = Ingredient('camarão')
+    ingredient2 = Ingredient('creme de leite')
+    ingredient3 = Ingredient('camarão')
+
+    # Teste de inicialização
+    assert ingredient.name == 'camarão'
+    assert ingredient.restrictions == {Restriction.SEAFOOD}
+
+    # Teste de representação
+    assert repr(ingredient2) == "Ingredient('creme de leite')"
+
+    # Teste de igualdade
+    assert ingredient == ingredient3
+    assert ingredient != ingredient2
+
+    # Teste de hash
+    assert hash(ingredient) == hash(ingredient3)
+    assert hash(ingredient) != hash(ingredient2)
